cac0eb9ff7e63db8b47084180518c07a1de6beba
Luís Serralheiro <encavadas@gmail.com>
2016-03-01 16:46:16 +0000

    Improving the handling of negative numbers

--- jquery-numeric.js
+++ jquery-numeric.js
@@ -5,28 +5,30 @@
  * Copyright 2012, Luís Serralheiro
  */
 (function ($) {
-	$.fn.numeric = function (opt) {
-	
-		var that=this;
-		var opts = {max: Number(this.attr('max')), min: Number(this.attr('min')), decimal: true};
-		$.extend(opts, opt);
-	
-		var currentNumber;
-	
-		this.bind('keypress paste drop', function (e) {
-			currentNumber = that.val();
-	
-			var validate = function (numberToValidate, defaultNumber) {
-				if (!isNaN(numberToValidate) && numberToValidate <= opts.max && numberToValidate >= opts.min) {
-					that.val(opts.decimal ? numberToValidate : Number(numberToValidate).toFixed(0));
-				} else {
-					that.val(defaultNumber);
-				}
-			};
-	
-			window.setTimeout(function () {
-				validate(that.val(), currentNumber)
-			}, 1);
-		});
-	};
+    $.fn.numeric = function (opt) {
+
+        var that = this;
+        var opts = {max: Number(this.attr('max')), min: Number(this.attr('min')), decimal: true};
+        $.extend(opts, opt);
+
+        var currentNumber;
+
+        this.bind('keypress paste drop', function (e) {
+            currentNumber = that.val();
+
+            var validate = function (numberToValidate, defaultNumber) {
+                if (!isNaN(numberToValidate) && numberToValidate <= opts.max && numberToValidate >= opts.min) {
+                    that.val(opts.decimal ? numberToValidate : Number(numberToValidate).toFixed(0));
+                } else {
+                    if (that.val() !== '-') {
+                        that.val(defaultNumber);
+                    }
+                }
+            };
+
+            window.setTimeout(function () {
+                validate(that.val(), currentNumber);
+            }, 1);
+        });
+    };
 }(jQuery));
